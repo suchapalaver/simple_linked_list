2c60852ee79aca8c3bac74049bb2afb5bbbe17cf
“suchapalaver” <jlivesey@gmail.com>
2022-05-10 16:38:27 -0400

    Add basic idea for linked_list demo

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(val: i32) -> Node {
+	Node{ val, next: None }
+    }
+}
+
+fn to_list(vector: Vec<i32>) -> Option<Box<Node>> {
+    let mut cur = None;
+    for &value in vector.iter().rev() {
+	let mut new_node = Node::new(value);
+	new_node.next = cur;
+	cur = Some(Box::new(new_node));
+    }
+    cur
+}
+
+fn unbox<Node>(value: Box<Node>) -> Node {
+    *value
+}
+
+fn main() {
+    let _init_node = Node { val: 0, next: Some(Box::new(Node::new(1))) }; 
+    let vector = vec![0, 1, 2, 3]; 
+    for node in to_list(vector) {
+	
+	let new_node = unbox(node);
+	println!("node val: {:?} next: {:?}", new_node.val, new_node.next);
+    }
+}
